f65e89e971506c2b1b67f0b24a8624df520ece80
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-13 17:10:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,6 @@
 def my_each(names)# put argument(s) here
-  names = ["shahid" , "rokhaya" ,"fama"]
-  my_each(names) do |name|# code here
-  puts name += 1
+  names = ["shahid","rokhaya" ,"fama".]
+  my_each(names) do|name|# code here
+  puts name
 end
 end 
